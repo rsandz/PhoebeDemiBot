a4be58b96ce215e01e02b009625bf0a4cbe1efc3
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2022-03-03 10:52:16 -0700

    formatting + security

--- PhoebeDemi.py
+++ PhoebeDemi.py
@@ -10,7 +10,7 @@ import pytz
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = '.', intents=intents)
+client = commands.Bot(command_prefix='.', intents=intents)
 
 load_dotenv()
 gender_message = os.getenv('GENDER_MESSAGE')
@@ -18,7 +18,7 @@ creator_message = os.getenv('CREATOR_MESSAGE')
 colour_message = os.getenv('COLOUR_MESSAGE')
 welcome_channel = os.getenv('WELCOME_CHANNEL')
 rules_channel = os.getenv('RULES_CHANNEL')
-roles_channel = os.getenv('ROLES_CHANNEL') 
+roles_channel = os.getenv('ROLES_CHANNEL')
 intro_channel = os.getenv('INTRO_CHANNEL')
 announce_channel = os.getenv('ANNOUNCE_CHANNEL')
 server_id = os.getenv('SERVER_ID')
@@ -26,17 +26,17 @@ server_id = os.getenv('SERVER_ID')
 messageList = {}
 
 gender_roles = {
-    '❤️' : 'he/him',
-    '🧡' : 'he/they',
-    '💛' : 'they/them',
-    '💚' : 'she/they',
-    '💙' : 'she/her'
+    '❤️': 'he/him',
+    '🧡': 'he/they',
+    '💛': 'they/them',
+    '💚': 'she/they',
+    '💙': 'she/her'
 }
 gender_emoji_list = gender_roles.keys()
 
 colour_roles = {
-    '✨' : 'From Another Realm',
-    '🚀' : 'From the Far Future'
+    '✨': 'From Another Realm',
+    '🚀': 'From the Far Future'
 }
 colour_emoji_list = colour_roles.keys()
 
@@ -50,7 +50,7 @@ answers = [
     "definitely not!"
 ]
 
-tzinfo = pytz.timezone("America/Edmonton")
+tzinfo = pytz.timezone(os.getenv('LOCATION'))
 
 
 # ========== HELPER FUNCTIONS ========== #
@@ -69,7 +69,8 @@ def getStaged():
         holder = ""
         for i in range(len(lines)):
             if i % 2 == 0:
-                holder =  datetime.strptime(lines[i][:-7], "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
+                holder = datetime.strptime(
+                    lines[i][:-7], "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
             else:
                 messageList[holder] = lines[i].rstrip()
 
@@ -84,6 +85,8 @@ async def on_ready():
     sendTimedMessages.start()
 
 # Sneding a message when a member joins
+
+
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(int(welcome_channel))
@@ -92,9 +95,11 @@ async def on_member_join(member):
     role = client.get_channel(int(roles_channel))
 
     message = "Hello {member.mention}! Welcome to the Official Warp Speed and Witchcraft Server! Consider checking out {rules.mention}, giving yourself a role in {role.mention}, and introducing yourself in {intro.mention}!"
-    await channel.send(message.format(member = member, rules = rules, intro = intro, role = role))
+    await channel.send(message.format(member=member, rules=rules, intro=intro, role=role))
 
 # Detection for roles pickers
+
+
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
@@ -114,10 +119,10 @@ async def on_raw_reaction_add(payload):
         if emoji == '📝':
             role = get(server.roles, name="Creator")
             await member.add_roles(role)
-    
+
     # colour roles
     if message_id == int(colour_message):
-        future_role =  get(server.roles, name='From the Far Future')
+        future_role = get(server.roles, name='From the Far Future')
         realm_role = get(server.roles, name='From Another Realm')
         message = await channel.fetch_message(payload.message_id)
         if emoji == '✨':
@@ -130,9 +135,9 @@ async def on_raw_reaction_add(payload):
                 await member.remove_roles(realm_role)
                 await message.remove_reaction('✨', member)
             await member.add_roles(future_role)
-    
 
-# Detection for removing a role 
+
+# Detection for removing a role
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
@@ -145,13 +150,13 @@ async def on_raw_reaction_remove(payload):
         if emoji in gender_emoji_list:
             role = get(server.roles, name=gender_roles[emoji])
             await member.remove_roles(role)
-    
+
     # creator role
     if message_id == int(creator_message):
         if emoji == '📝':
             role = get(server.roles, name="Creator")
             await member.remove_roles(role)
-    
+
     # colour roles
     if message_id == int(colour_message):
         if emoji in colour_emoji_list:
@@ -170,8 +175,9 @@ async def question(ctx, *args):
 
 # ========== QUOTES ========== #
 
+
 @client.command()
-async def quote(ctx, *args): 
+async def quote(ctx, *args):
     n = 1
     if len(args) > 0 and str.isdigit(args[0]):
         if (int(args[0]) > 5):
@@ -190,6 +196,7 @@ async def quote(ctx, *args):
 async def quotes(ctx):
     await ctx.send(file=discord.File('quotes.txt'))
 
+
 @client.command()
 @commands.has_role('Ya Boi')
 async def newquote(ctx, *args):
@@ -200,11 +207,13 @@ async def newquote(ctx, *args):
     f.close()
     await ctx.channel.send("new quote added: `" + line + "`")
 
-#https://stackoverflow.com/questions/1877999/delete-final-line-in-file-with-python
+# https://stackoverflow.com/questions/1877999/delete-final-line-in-file-with-python
+
+
 @client.command()
 @commands.has_role('Ya Boi')
 async def deletequote(ctx, *args):
-    with open('quotes.txt', "r+", encoding = "utf-8") as file:
+    with open('quotes.txt', "r+", encoding="utf-8") as file:
         file.seek(0, os.SEEK_END)
         pos = file.tell() - 1
         while pos > 0 and file.read(1) != "\n":
@@ -217,18 +226,21 @@ async def deletequote(ctx, *args):
 
 # ========== RECOMMENDS ========== #
 
+
 @client.command()
-async def recommend(ctx, *args): 
+async def recommend(ctx, *args):
     with open('recommend.txt', 'r') as f:
         recs = [line.strip() for line in f]
         choice = random.randint(0, (len(recs) / 2) - 1)
         await ctx.channel.send(recs[choice * 2])
-        await ctx.channel.send(recs[(choice * 2)+ 1])
-      
+        await ctx.channel.send(recs[(choice * 2) + 1])
+
+
 @client.command()
 @commands.has_role('Ya Boi')
 async def recommends(ctx):
     await ctx.send(file=discord.File('recommend.txt'))
+
 
 @client.command()
 @commands.has_role('Ya Boi')
@@ -239,11 +251,13 @@ async def newrec(ctx, *args):
     f.close()
     await ctx.channel.send("new rec added: `" + lines[0] + " \n " + lines[1] + "`")
 
-#https://stackoverflow.com/questions/1877999/delete-final-line-in-file-with-python
+# https://stackoverflow.com/questions/1877999/delete-final-line-in-file-with-python
+
+
 @client.command()
 @commands.has_role('Ya Boi')
 async def deleterec(ctx, *args):
-    with open('recommend.txt', "r+", encoding = "utf-8") as file:
+    with open('recommend.txt', "r+", encoding="utf-8") as file:
         for i in range(2):
             file.seek(0, os.SEEK_END)
             pos = file.tell() - 1
@@ -257,37 +271,40 @@ async def deleterec(ctx, *args):
 
 # ========== STAGED MESSAGES ========== #
 
+
 @client.command()
 @commands.has_role('Ya Boi')
 async def stage(ctx, *args):
     if len(args) <= 2:
-        await ctx.channel.send("Not enough arguments" )
+        await ctx.channel.send("Not enough arguments")
         return
 
     date = args[0]
     time = args[1]
 
     try:
-        dateTime = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
+        dateTime = datetime.strptime(
+            date + " " + time, "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
     except:
-        await ctx.channel.send("Date and time incorrect. Format: `y-m-d h:m:s`" )
+        await ctx.channel.send("Date and time incorrect. Format: `y-m-d h:m:s`")
         return
 
     message = ""
     if args[2] == "$":
-        message = "@everyone, A new episode of Warp Speed and Witchcraft is out! https://www.webtoons.com/en/challenge/warp-speed-and-witchcraft/convention-con-4/viewer?title_no=644747&episode_no=" + args[3]
+        message = "@everyone, A new episode of Warp Speed and Witchcraft is out! https://www.webtoons.com/en/challenge/warp-speed-and-witchcraft/convention-con-4/viewer?title_no=644747&episode_no=" + \
+            args[3]
 
     else:
-        for i in  range(2, len(args)):
+        for i in range(2, len(args)):
             if args[i] == "@":
-                 message = message + "@everyone "
+                message = message + "@everyone "
             else:
                 message = message + args[i] + " "
-    
-    allowed_mentions = discord.AllowedMentions(everyone = True)
+
+    allowed_mentions = discord.AllowedMentions(everyone=True)
     messageList[dateTime] = message
     storeStaged()
-    await ctx.channel.send("Staged Message: " + "`" + str(dateTime) + " : " + messageList[dateTime] + "`", allowed_mentions = allowed_mentions)
+    await ctx.channel.send("Staged Message: " + "`" + str(dateTime) + " : " + messageList[dateTime] + "`", allowed_mentions=allowed_mentions)
 
 
 @client.command()
@@ -297,7 +314,7 @@ async def stagelist(ctx):
         await ctx.channel.send("`No messages staged`")
         return
     for message in messageList:
-        await ctx.channel.send("`" + str(message) + " : " + messageList[message] + "`" )
+        await ctx.channel.send("`" + str(message) + " : " + messageList[message] + "`")
 
 
 @client.command()
@@ -306,17 +323,18 @@ async def dropstage(ctx, *args):
     date = args[0]
     time = args[1]
     try:
-        dateTime = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
+        dateTime = datetime.strptime(
+            date + " " + time, "%Y-%m-%d %H:%M:%S").astimezone(tzinfo)
         del messageList[dateTime]
     except:
         await ctx.channel.send("That doesn't exist")
 
-    await ctx.channel.send("Stage List:" )
+    await ctx.channel.send("Stage List:")
     if len(messageList) == 0:
         await ctx.channel.send("`No messages staged`")
         return
     for message in messageList:
-        await ctx.channel.send("`" + str(message) + " : " + messageList[message] + "`" )
+        await ctx.channel.send("`" + str(message) + " : " + messageList[message] + "`")
 
 
 @tasks.loop(seconds=30.0)
@@ -334,16 +352,15 @@ async def sendTimedMessages():
 # ========== OTHER ========== #
 
 # https://stackoverflow.com/questions/43465082/python-discord-py-delete-all-messages-in-a-text-channel
-@client.command(pass_context = True)
+@client.command(pass_context=True)
 @commands.has_role('Ya Boi')
-async def clear(ctx, amount = 1000):
+async def clear(ctx, amount=1000):
     await ctx.channel.purge(limit=amount)
 
 
 @client.command()
 @commands.has_role('Ya Boi')
 async def time(ctx):
-     await ctx.channel.send("The current bot time is: `" + str(datetime.now(tzinfo)) + "`")
+    await ctx.channel.send("The current bot time is: `" + str(datetime.now(tzinfo)) + "`")
 
 client.run(os.getenv('DISCORD_TOKEN'))
-
